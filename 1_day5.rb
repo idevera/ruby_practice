5b73cd0b661985e474895ea6e7b5f27383599683
Irene <irenneeyyy@gmail.com>
2017-07-22 16:37:02 -0700

    First commit arrays

---
+++ 1_day5.rb
@@ -0,0 +1,13 @@
+all_names = Array.new
+
+5.times do
+puts "Please enter five names"
+  input_name = gets.chomp.upcase
+  all_names.push(input_name)
+end
+
+i = 0
+until all_names[i] == all_names.length
+  print all_names[i]
+  i += 1
+end
